e45045b8cbf7bf621b0f1af35e9c9c633f63acba
z4ku44 <alternative2k@googlemail.com>
2021-11-05 12:19:41 +0100

    add user entity

---
+++ backend/src/main/java/io/backend/model/UserEntity.java
@@ -0,0 +1,26 @@
+package io.backend.model;
+
+import java.util.Date;
+
+public class UserEntity {
+
+    private String userId;
+    private String userName;
+    private String password;
+    private String userRole;
+
+    private String firstName;
+    private String lastName;
+    private int age;
+    private String gender;
+    private String location;
+    private String aboutMe;
+
+    private Date drivingSince;
+    private String drivingExp;
+    private String drivingStyle;
+
+    private Date registeredOn;
+    private Date deletedOn;
+
+}
